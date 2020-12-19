29eac8384b1ca54ad49082d2117a72c4934c8be4
Hioken <hioki.kenta.infratop@gmail.com>
2020-12-19 19:25:20 +0900

    Regexp

--- chapter6_Regular_expression/test_Regexp.rb
+++ chapter6_Regular_expression/test_Regexp.rb
@@ -49,7 +49,72 @@ TEXT
 
 puts text.gsub(/:\s*/, ': ')
 
+text = <<TEXT
+name,email
+alice,alice@example.com
+bob,bob@example.com
+TEXT
+
+puts text.gsub(/,/, "\t")
+
+
+text = <<TEXT
+Feb 14 07:33:02 app/web.1:  Completed 302 Found ...
+Feb 14 07:36:46 heroku/api:  Starting process ...
+Feb 14 07:36:50 heroku/scheduler.7625:  Starting ...
+Feb 14 07:36:50 heroku/scheduler.7625:  State ...
+Feb 14 07:36:54 heroku/router:  at=info method=...
+Feb 14 07:36:54 app/web.1:  Started HEAD "/" ...
+Feb 14 07:36:54 app/web.1:  Completed 200 ...
+TEXT
+
+puts '結果1'
+puts text.scan(/^.+(app|router).+$/).join("\n")
+puts "\n結果2"
+puts text.gsub(/^.+heroku\/(api|scheduler).+\n/, '')
+
+
+text = <<TEXT
+Feb 14 07:33:02 app/web.1:  Completed 302 Found ...
+Feb 14 07:36:46 heroku/api:  Starting process ...
+Feb 14 07:36:50 heroku/scheduler.7625:  Starting ...
+Feb 14 07:36:50 heroku/scheduler.7625:  State ...
+Feb 14 07:36:54 heroku/router:  at=info method=...
+Feb 14 07:36:54 app/web.1:  Started HEAD "/" ...
+Feb 14 07:36:54 app/web.1:  Completed 200 ...
+TEXT
+
+puts '結果1'
+puts text.scan(/^.+(?:app|router).+$/).join("\n")
+puts "\n結果2"
+puts text.gsub(/^.+heroku\/(api|scheduler).+\n/, '')
+
+
+text = <<-TEXT
+type=zip; filename=users.zip; size=1024;
+type=xml; filename=posts.xml; size=2048;
+TEXT
+p text.scan(/filename=([^;]+)/).map{|s| s.last}
+
+
+text = "私の誕生日は1997年7月17日です"
+data = text.match(/(\d+)年(\d+)月(\d+)日/)
+p data.class
+p data[1..3].class
+
+data2 = /(\d+)年(\d+)月(\d+)日/.match(text)
+p data2
+
+
+text = "私の誕生日は1997年7月17日です"
+/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+p year #=> "1997"
+
+
+p 'ab,cd-ef'.split(/,|\-/)
 =end
 
-hash = {:a => {:b => 'c'}}
-p hash
+a = /\d{3}/ =~ '111'
+b = 1 == 1
+p a
+p b
